0a145e83812a037a3eadb6fa03eaabb73fb34409
camilakbn <canmilabrito@gmail.com>
2026-09-09 11:36:48 +0100

    mais updates

--- jogoDoGaloOk.cpp
+++ jogoDoGaloOk.cpp
@@ -9,19 +9,6 @@ tema: jogo do galo (jogo da velha)
 entrega do código: 11/09
 apresentação: 16/09*/
 
-void guardarJogador(ofstream &ficheiro, string nomeJogador, int numPartidas,
-                    int vitorias, int derrotas, int empates)
-{
-    ficheiro << nomeJogador << " " << numPartidas << " "
-             << vitorias << " " << derrotas << " " << empates << endl;
-}
-
-bool mostrarJogador(ifstream &ficheiro, string &nomeJogador, int &numPartidas,
-                    int &vitorias, int &derrotas, int &empates)
-{
-    return bool(ficheiro >> nomeJogador >> numPartidas >> vitorias >> derrotas >> empates);
-}
-
 void mostrarTabuleiro(char jogo[3][3])
 {
     for (int i = 0; i < 3; i++)
@@ -43,6 +30,184 @@ void mostrarTabuleiro(char jogo[3][3])
     }
 }
 
+void guardarJogador(string nomeJogador, int partida, int vitoria, int derrota, int empate)
+{
+    string nomes[100];
+    int partidas[100], vitorias[100], derrotas[100], empates[100];
+    int total = 0;
+
+    ifstream entrada("jogoDoGalo.txt");
+    if (entrada.is_open())
+    {
+        while (entrada >> nomes[total] >> partidas[total] >> vitorias[total] >> derrotas[total] >> empates[total])
+        {
+            total++;
+        }
+        entrada.close();
+    }
+
+    int encontrado = 0;
+    for (int i = 0; i < total; i++)
+    {
+        if (nomes[i] == nomeJogador)
+        {
+            partidas[i] = partidas[i] + 1;
+            vitorias[i] = vitorias[i] + vitoria;
+            derrotas[i] = derrotas[i] + derrota;
+            empates[i] = empates[i] + empate;
+            encontrado = 1;
+            break;
+        }
+    }
+
+    if (encontrado == 0)
+    {
+        nomes[total] = nomeJogador;
+        partidas[total] = 1;
+        vitorias[total] = vitoria;
+        derrotas[total] = derrota;
+        empates[total] = empate;
+        total++;
+    }
+
+    ofstream saida("jogoDoGalo.txt");
+    if (saida.is_open())
+    {
+        for (int i = 0; i < total; i++)
+        {
+            saida << nomes[i] << " " << partidas[i] << " "
+                  << vitorias[i] << " " << derrotas[i] << " " << empates[i] << endl;
+        }
+        saida.close();
+    }
+}
+
+bool mostrarJogador(ifstream &ficheiro, string &nomeJogador, int &numPartidas,
+                    int &vitorias, int &derrotas, int &empates)
+{
+    if (!ficheiro.is_open())
+    {
+        cout << "Erro ao abrir ficheiro\n";
+        return false;
+    }
+    else
+    {
+        cout << "Estatisticas: \n";
+
+        while (ficheiro >> nomeJogador >> numPartidas >> vitorias >> derrotas >> empates)
+        {
+            cout << "Jogador -> " << nomeJogador << ": Partidas: " << numPartidas << " | Vitorias: " << vitorias << " | Derrotas: " << derrotas << " | Empates: " << empates << endl;
+        }
+        ficheiro.close();
+        return true;
+    }
+}
+
+void jogadaComputadorInteligente(char jogo[3][3], char pcInteligente, char jogador)
+{
+    int jaJogou = 0;
+
+    if (jaJogou == 0)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (jogo[i][j] == ' ')
+                {
+                    jogo[i][j] = pcInteligente;
+                    if ((jogo[i][0] == pcInteligente && jogo[i][1] == pcInteligente && jogo[i][2] == pcInteligente) ||
+                        (jogo[0][j] == pcInteligente && jogo[1][j] == pcInteligente && jogo[2][j] == pcInteligente) ||
+                        (jogo[0][0] == pcInteligente && jogo[1][1] == pcInteligente && jogo[2][2] == pcInteligente) ||
+                        (jogo[0][2] == pcInteligente && jogo[1][1] == pcInteligente && jogo[2][0] == pcInteligente))
+                    {
+                        jaJogou = 1;
+                        break;
+                    }
+                    jogo[i][j] = ' ';
+                }
+            }
+            if (jaJogou == 1)
+                break;
+        }
+    }
+
+    if (jaJogou == 0)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (jogo[i][j] == ' ')
+                {
+                    jogo[i][j] = jogador;
+                    if ((jogo[i][0] == jogador && jogo[i][1] == jogador && jogo[i][2] == jogador) ||
+                        (jogo[0][j] == jogador && jogo[1][j] == jogador && jogo[2][j] == jogador) ||
+                        (jogo[0][0] == jogador && jogo[1][1] == jogador && jogo[2][2] == jogador) ||
+                        (jogo[0][2] == jogador && jogo[1][1] == jogador && jogo[2][0] == jogador))
+                    {
+                        jogo[i][j] = pcInteligente;
+                        jaJogou = 1;
+                        break;
+                    }
+                    jogo[i][j] = ' ';
+                }
+            }
+            if (jaJogou == 1)
+            {
+                break;
+            }
+        }
+    }
+
+    if (jaJogou == 0 && jogo[1][1] == ' ')
+    {
+        jogo[1][1] = pcInteligente;
+        jaJogou = 1;
+    }
+
+    if (jaJogou == 0 && jogo[0][0] == ' ')
+    {
+        jogo[0][0] = pcInteligente;
+        jaJogou = 1;
+    }
+    if (jaJogou == 0 && jogo[0][2] == ' ')
+    {
+        jogo[0][2] = pcInteligente;
+        jaJogou = 1;
+    }
+    if (jaJogou == 0 && jogo[2][0] == ' ')
+    {
+        jogo[2][0] = pcInteligente;
+        jaJogou = 1;
+    }
+    if (jaJogou == 0 && jogo[2][2] == ' ')
+    {
+        jogo[2][2] = pcInteligente;
+        jaJogou = 1;
+    }
+
+    if (jaJogou == 0)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (jogo[i][j] == ' ')
+                {
+                    jogo[i][j] = pcInteligente;
+                    jaJogou = 1;
+                    break;
+                }
+            }
+            if (jaJogou == 1)
+            {
+                break;
+            }
+        }
+    }
+}
+
 int main()
 {
 
@@ -53,18 +218,11 @@ int main()
     string nomeJogador1 = "";
     string nomeJogador2 = "";
     string nomeJogador = "";
-    int numPartidasJ1 = 0;
-    int numPartidasJ2 = 0;
-    int vitoriasJ1 = 0;
-    int vitoriasJ2 = 0;
-    int derrotasJ1 = 0;
-    int derrotasJ2 = 0;
-    int empatesJ1 = 0;
-    int empatesJ2 = 0;
     int finalJogo;
     int opcao;
     char posicao;
     int nivelBoot;
+    char pcInteligente = 'O';
 
     do
     {
@@ -99,7 +257,7 @@ int main()
 
                     if (jogada % 2 == 0)
                     {
-                        jogadorAtual = jogador1; // descobrir como colocar o nome do jogador
+                        jogadorAtual = jogador1;
                         cout << "\n\n"
                              << nomeJogador1 << ", ";
                     }
@@ -118,6 +276,7 @@ int main()
                     cout << "Escolha a coluna (entre 1 e 3): ";
                     cin >> coluna;
 
+                    // 1 funcao
                     while (linha < 1 || linha > 3 || coluna < 1 || coluna > 3 || *(*(jogo + (linha - 1)) + (coluna - 1)) != ' ')
                     {
 
@@ -137,11 +296,15 @@ int main()
                         cin >> linha;
                         cout << "Escolha a coluna (entre 1 e 3): \n";
                         cin >> coluna;
-                        // descobrir como tirar esse codigo repetido
                     }
+                    // ate aqui
 
                     *(*(jogo + (linha - 1)) + (coluna - 1)) = jogadorAtual;
 
+                    cout << "\n";
+                    mostrarTabuleiro(jogo);
+
+                    // 1 funcao vencedor
                     if (
                         (jogo[0][0] == jogadorAtual && jogo[0][1] == jogadorAtual && jogo[0][2] == jogadorAtual) ||
                         (jogo[1][0] == jogadorAtual && jogo[1][1] == jogadorAtual && jogo[1][2] == jogadorAtual) ||
@@ -158,71 +321,165 @@ int main()
                         {
                             cout << "\n"
                                  << nomeJogador1 << " venceu!\n";
-                            numPartidasJ1++;
-                            vitoriasJ1++;
-                            numPartidasJ2++;
-                            derrotasJ2++;
+
+                            mostrarTabuleiro(jogo);
+
+                            guardarJogador(nomeJogador1, 1, 1, 0, 0);
+                            guardarJogador(nomeJogador2, 1, 0, 1, 0);
+                            break;
                         }
                         else if (jogadorAtual == jogador2)
                         {
                             cout << "\n"
                                  << nomeJogador2 << " venceu!\n";
-                            numPartidasJ2++;
-                            vitoriasJ2++;
-                            numPartidasJ1++;
-                            derrotasJ1++;
-                        }
 
-                        ofstream ficheiro("jogoDoGalo.txt", ios::app);
-                        if (ficheiro.is_open())
-                        {
-                            guardarJogador(ficheiro, nomeJogador1, numPartidasJ1, vitoriasJ1, derrotasJ1, empatesJ1);
-                            guardarJogador(ficheiro, nomeJogador2, numPartidasJ2, vitoriasJ2, derrotasJ2, empatesJ2);
-                            ficheiro.close();
+                            mostrarTabuleiro(jogo);
+
+                            guardarJogador(nomeJogador1, 1, 0, 1, 0);
+                            guardarJogador(nomeJogador2, 1, 1, 0, 0);
+                            break;
                         }
-                        break;
                     }
+                    // ate aqui
 
+                    // 1 funcao empate
                     if (jogada == 8)
                     {
 
                         cout << "Empate!\n";
-                        numPartidasJ1++;
-                        numPartidasJ2++;
-                        empatesJ1++;
-                        empatesJ2++;
 
-                        ofstream ficheiro("jogoDoGalo.txt", ios::app);
-                        if (ficheiro.is_open())
-                        {
-                            guardarJogador(ficheiro, nomeJogador1, numPartidasJ1, vitoriasJ1, derrotasJ1, empatesJ1);
-                            guardarJogador(ficheiro, nomeJogador2, numPartidasJ2, vitoriasJ2, derrotasJ2, empatesJ2);
-                            ficheiro.close();
-                        }
                         mostrarTabuleiro(jogo);
+
+                        guardarJogador(nomeJogador1, 1, 0, 0, 1);
+                        guardarJogador(nomeJogador2, 1, 0, 0, 1);
                         break;
                     }
-
-                    cout << "\n\n";
-                    mostrarTabuleiro(jogo);
+                    // ate aqui
                 }
             }
             else if (opcao == 2)
             {
                 do
                 {
-                    cout << "Voce escolheu a opcao de jogar contra o computador\nVoce pode escolher entre dois niveis\n1 - Facil\n2 - Dificil\nEscolha um: ";
+                    cout << "Voce escolheu a opcao de jogar contra o computador\nVoce pode escolher entre dois niveis\n1 - Facil\n2 - Normal\nEscolha um: ";
                     cin >> nivelBoot;
-                    if (nivelBoot == 1){
+                    if (nivelBoot == 1)
+                    {
                         cout << "Bem-vindo ao nivel facil!\n";
-                        //add codigo
+
+                        // add codigo
                     }
-                    else if(nivelBoot == 2){
-                        cout << "Bem-vindo ao nivel dificil!\n";
-                        //add codigo
-                    }
-                    else{
-                        cout << "Escolha invalida!\n\n";
+                    else if (nivelBoot == 2)
+                    {
+                        cout << "Bem-vindo ao nivel normal!\n";
+
+                        char jogo[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+
+                        cout << "Insira o nome do jogador: ";
+                        getline(cin >> ws, nomeJogador1);
+
+                        cout << "O jogador sera o 'X' e o computador sera o 'O'\n\n";
+                        cout << "Aqui esta o jogo da velha:\n";
+                        mostrarTabuleiro(jogo);
+
+                        // add codigo
+                        for (int jogada = 0; jogada < 9; jogada++)
+                        {
+
+                            if (jogada % 2 == 0)
+                            {
+                                jogadorAtual = jogador1;
+                                cout << "\n\n"
+                                     << nomeJogador1 << ", qual a posicao que deseja realizar a jogada? ";
+                                cout << "Escolha a linha (entre 1 e 3): ";
+                                cin >> linha;
+                                cout << "Escolha a coluna (entre 1 e 3): ";
+                                cin >> coluna;
+
+                                // 1 funcao
+                                while (linha < 1 || linha > 3 || coluna < 1 || coluna > 3 || *(*(jogo + (linha - 1)) + (coluna - 1)) != ' ')
+                                {
+
+                                    if (linha < 1 || linha > 3 || coluna < 1 || coluna > 3)
+                                    {
+
+                                        cout << "\n\nPosicao invalida! Escolha outra posicao!";
+                                    }
+
+                                    else if (*(*(jogo + (linha - 1)) + (coluna - 1)) != ' ')
+                                    {
+                                        cout << "\n\nPosicao ocupada! Escolha outra posicao!";
+                                    }
+
+                                    cout << "\n\nQual a posicao que deseja realizar a jogada?";
+                                    cout << "Escolha a linha (entre 1 e 3):";
+                                    cin >> linha;
+                                    cout << "Escolha a coluna (entre 1 e 3): \n";
+                                    cin >> coluna;
+                                }
+                                // ate aqui
+
+                                *(*(jogo + (linha - 1)) + (coluna - 1)) = jogadorAtual;
+                            }
+
+                            else
+                            {
+                                cout << "\n\nJogada do computador: \n";
+                                jogadorAtual = pcInteligente;
+                                jogadaComputadorInteligente(jogo, pcInteligente, jogador1);
+                            }
+
+                            cout << "\n";
+                            mostrarTabuleiro(jogo);
+
+                            // 1 funcao vencedor
+                            if (
+                                (jogo[0][0] == jogadorAtual && jogo[0][1] == jogadorAtual && jogo[0][2] == jogadorAtual) ||
+                                (jogo[1][0] == jogadorAtual && jogo[1][1] == jogadorAtual && jogo[1][2] == jogadorAtual) ||
+                                (jogo[2][0] == jogadorAtual && jogo[2][1] == jogadorAtual && jogo[2][2] == jogadorAtual) ||
+
+                                (jogo[0][0] == jogadorAtual && jogo[1][0] == jogadorAtual && jogo[2][0] == jogadorAtual) ||
+                                (jogo[0][1] == jogadorAtual && jogo[1][1] == jogadorAtual && jogo[2][1] == jogadorAtual) ||
+                                (jogo[0][2] == jogadorAtual && jogo[1][2] == jogadorAtual && jogo[2][2] == jogadorAtual) ||
+
+                                (jogo[0][0] == jogadorAtual && jogo[1][1] == jogadorAtual && jogo[2][2] == jogadorAtual) ||
+                                (jogo[0][2] == jogadorAtual && jogo[1][1] == jogadorAtual && jogo[2][0] == jogadorAtual))
+                            {
+                                if (jogadorAtual == jogador1)
+                                {
+                                    cout << "\n"
+                                         << nomeJogador1 << " venceu!\n";
+
+                                    mostrarTabuleiro(jogo);
+
+                                    guardarJogador(nomeJogador1, 1, 1, 0, 0);
+                                    break;
+                                }
+                                else if (jogadorAtual == pcInteligente)
+                                {
+                                    cout << "\nO computador venceu!\n";
+
+                                    mostrarTabuleiro(jogo);
+
+                                    guardarJogador(nomeJogador1, 1, 0, 1, 0);
+                                    break;
+                                }
+                            }
+                            // ate aqui
+
+                            // 1 funcao empate
+                            if (jogada == 8)
+                            {
+
+                                cout << "\n\nEmpate!\n";
+
+                                mostrarTabuleiro(jogo);
+
+                                guardarJogador(nomeJogador1, 1, 0, 0, 1);
+                                break;
+                            }
+                            // ate aqui
+                        }
                     }
                 } while (nivelBoot < 1 || nivelBoot > 2);
             }
@@ -237,16 +494,14 @@ int main()
                 if (!ficheiro.is_open())
                 {
                     cout << "Erro ao abrir ficheiro\n";
-                    return 1;
+                    break;
                 }
                 else
                 {
-                    cout << "Estatisticas: \n";
 
-                    while (ficheiro >> nomeJogador >> numPartidas >> vitorias >> derrotas >> empates)
-                    {
-                        cout << "Jogador -> " << nomeJogador << ": Partidas: " << numPartidas << " | Vitorias: " << vitorias << " | Derrotas: " << derrotas << " | Empates: " << empates << endl;
-                    }
+                    mostrarJogador(ficheiro, nomeJogador, numPartidas,
+                                   vitorias, derrotas, empates);
+
                     ficheiro.close();
                 }
             }
